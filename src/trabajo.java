4a925f32ca84c538aeb4bca1610dda4c36eb4235
nicol899 <email>
2025-04-23 14:55:30 -0500

    Tulepera

--- src/trabajo.java
+++ src/trabajo.java
@@ -3,6 +3,8 @@ public class trabajo {
     public static void main(String[] args) {
         System.out.println("Wenassss");
         int tuteca = 0;
+        System.out.println("Tulepera con la papaya");
+        
 
     }
 }
