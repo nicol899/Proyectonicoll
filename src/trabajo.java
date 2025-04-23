cd3fab63bfe74e9a3d9a795e2e945bc4f21f41b5
nicol899 <email>
2025-04-23 14:36:38 -0500

    trabajo

--- src/trabajo.java
+++ src/trabajo.java
@@ -1,6 +1,9 @@
-public static void main(String[] args) {
-    class Innertrabajo {
-    
+public class trabajo {
+
+    public static void main(String[] args) {
+        System.out.println("Wenassss");
         
+
     }
 }
+    
